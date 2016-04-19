0a87bb1603fbee26a983acc671b8943e625f3c12
Han Bing Feng <hangoglebf@gmail.com>
2016-04-19 14:19:39 +0800

    sync with nirvana-workspace

--- src/core/validations.js
+++ src/core/validations.js
@@ -22,6 +22,9 @@ define(function (require) {
         return (str.length + (!b ? 0: b.length)); 
     }
 
+    function matchRegexp(value, regexp) {
+        return !isExisty(value) || isEmpty(value) || regexp.test(value);
+    }
 
     return {
 
@@ -34,7 +37,7 @@ define(function (require) {
 
         matchRegexp: function (value, regexp) {
             return {
-                isValid: !isExisty(value) || isEmpty(value) || regexp.test(value)
+                isValid: matchRegexp(value, regexp)
             };
         },
 
@@ -47,7 +50,7 @@ define(function (require) {
         isEmail: function (value) {
             var reg = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
             return {
-                isValid: this.matchRegexp(value, reg),
+                isValid: matchRegexp(value, reg),
                 template: '请输入正确的邮件地址'
             };
         },
@@ -55,7 +58,7 @@ define(function (require) {
         isUrl: function (value) {
             var reg = /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
             return {
-                isValid: this.matchRegexp(value, reg),
+                isValid: matchRegexp(value, reg),
                 template: '请输入正确的URL地址'
             };
         },
@@ -66,7 +69,7 @@ define(function (require) {
                 isValid = true;
             }
             else {
-                isValid = this.matchRegexp(value, /^[-+]?(?:\d*[.])?\d+$/);
+                isValid = matchRegexp(value, /^[-+]?(?:\d*[.])?\d+$/);
             }
             return {
                 isValid: isValid,
@@ -76,14 +79,14 @@ define(function (require) {
 
         isAlpha: function (value) {
             return {
-                isValid: this.matchRegexp(value, /^[A-Z]+$/i),
+                isValid: matchRegexp(value, /^[A-Z]+$/i),
                 template: '请输入英文字母'
             };
         },
 
         isAlphanumeric: function (value) {
             return {
-                isValid: this.matchRegexp(value, /^[0-9A-Z_]+$/i),
+                isValid: matchRegexp(value, /^[0-9A-Z_]+$/i),
                 template: '请勿使用除字母、数字和英文下划线外的其他字符。'
             };
         },
