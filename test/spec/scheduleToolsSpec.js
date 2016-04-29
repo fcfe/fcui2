3d2627412d0cca979d56f17efa3aed022d59b34b
Han Bing Feng <hangoglebf@gmail.com>
2016-04-29 16:25:37 +0800

    import karma and inited; dummy test added
    Change-Id: I756d2f8b025b8bc44046f7bc44020ac78d6fcfd0

---
+++ test/spec/scheduleToolsSpec.js
@@ -0,0 +1,12 @@
+/**
+ * @file spec for scheduleTools
+ * @author Han Bing Feng
+ */
+
+define(function (require) {
+    describe('dummy test', function () {
+        it('is a dummy test', function () {
+            expect(1).toBe(1);
+        });
+    });
+});
