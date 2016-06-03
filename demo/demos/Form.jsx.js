92322256d86b3ff21202c48a4ae8c301320a5902
lbxx1984 <279641976@qq.com>
2016-06-03 21:25:27 +0800

    finish form demo

--- demo/demos/Form.jsx.js
+++ demo/demos/Form.jsx.js
@@ -22,6 +22,14 @@ define(function (require) {
                 form2: {
                     name: 'Brian Li',
                     age: 18
+                },
+                form3: {
+                    name: 'Brian Li',
+                    age: 18
+                },
+                form4: {
+                    name: 'Brian Li',
+                    age: 20
                 }
             };
         },
@@ -32,11 +40,16 @@ define(function (require) {
                 validationResults: e.validationResults
             }));
         },
-        form2FieldChange: function (e) {
-            var form2 = JSON.parse(JSON.stringify(this.state.form2));
-            form2[e.field] = e.dataset[e.field];
-            this.setState({form2: form2});
-            this.alertEventElement(e);
+        formFieldChangeFactory: function (form) {
+            var me = this;
+            return function (e) {
+                var formdata = JSON.parse(JSON.stringify(me.state[form]));
+                formdata[e.field] = e.dataset[e.field];
+                var obj = {};
+                obj[form] = formdata;
+                me.setState(obj);
+                me.alertEventElement(e);
+            }
         },
         render: function () {
             return (
@@ -50,18 +63,18 @@ define(function (require) {
                     </div>
                     <div className="demo-item">
                         <h3>Form with field change handler</h3>
-                        <Form onFieldChange={this.form2FieldChange}>
+                        <Form onFieldChange={this.formFieldChangeFactory('form2')}>
                             <span>name: </span><TextBox name="name" value={this.state.form2.name}/><br/>
                             <span>age: </span><NumberBox name="age" value={this.state.form2.age}/>
                         </Form>
                     </div>
                     <div className="demo-item">
                         <h3>Form with field validations</h3>
-                        <Form onFieldChange={this.form2FieldChange}>
+                        <Form onFieldChange={this.formFieldChangeFactory('form3')}>
                             <span>name: </span>
-                            <TextBox name="name" value={this.state.form2.name}/><br/>
+                            <TextBox name="name" value={this.state.form3.name}/><br/>
                             <span>age: </span>
-                            <NumberBox name="age" value={this.state.form2.age}
+                            <NumberBox name="age" value={this.state.form3.age}
                                 validations={{
                                     maxAge: function (e) {
                                         return e < 21 ? true : '真不能大于20岁！'
@@ -69,6 +82,29 @@ define(function (require) {
                                 }}
                             />
                             <label>年龄不能大于20岁了</label>
+                        </Form>
+                    </div>
+                    <div className="demo-item">
+                        <h3>Form with cross validations</h3>
+                        <Form onFieldChange={this.formFieldChangeFactory('form4')}  validations={{
+                            maxAge: function (e) {
+                                if (
+                                    e.hasOwnProperty('name')
+                                    && e.hasOwnProperty('age')
+                                    && e.name === 'Brian Li'
+                                    && e.age > 18
+                                ) {
+                                    return '这输入违反表单校验规则';
+                                }
+                                return true;
+                            }
+                        }}>
+                            <label>Brian Li永远不超过18岁，点确定校验。</label><br/>
+                            <span>name: </span>
+                            <TextBox name="name" value={this.state.form4.name}/><br/>
+                            <span>age: </span>
+                            <NumberBox name="age" value={this.state.form4.age}/><br/>
+                            <Button label="确定" type="submit"/>
                         </Form>
                     </div>
                 </div>
