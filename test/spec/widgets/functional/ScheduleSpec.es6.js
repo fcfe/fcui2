9f2acb97dd5a8a666b893baea3c2f9530cd599b8
lbxx1984 <279641976@qq.com>
2016-07-10 13:49:04 +0800

    finished schedule UT

--- test/spec/widgets/functional/ScheduleSpec.es6.js
+++ test/spec/widgets/functional/ScheduleSpec.es6.js
@@ -12,7 +12,7 @@ define(function (require) {
     const ReactDOM = require('react-dom');
     const TestUtils = React.addons.TestUtils;
     const Schedule = require('Schedule.jsx');
-
+    const util = require('core/util');
 
     function realRender(Component, props) {
         return TestUtils.renderIntoDocument(<Component {...props} />);
@@ -28,37 +28,62 @@ define(function (require) {
             });
 
             it('mouse move', () => {
-                let dom = realRender(Schedule, {
-                    value: '[1,1,"",""]'
-                });
-                dom.onOptMouseMove({
-                    clientX: 10,
-                    clientY: 10
-                });
+                let dom = realRender(Schedule, {value: '[1,1,"",""]'});
+                dom.onOptMouseMove({clientX: 10, clientY: 10});
                 expect(dom.refs.legendLayer.___layerContainer___.childNodes[0].innerHTML).toBe('1');
-                dom.onOptMouseMove({
-                    clientX: 50,
-                    clientY: 10
-                });
+                dom.onOptMouseMove({clientX: 50, clientY: 10});
                 expect(dom.refs.legendLayer.___layerContainer___.childNodes[0].innerHTML).toBe('2:00 - 4:00');
-                dom.onOptMouseMove({
-                    clientX: 50,
-                    clientY: 80
-                });
+                dom.onOptMouseMove({clientX: 50, clientY: 80});
                 expect(dom.refs.legendLayer.___layerContainer___.childNodes[0].innerHTML).toBe('2:00 - 3:00');
                 dom.onOptMouseOut();
                 expect(dom.refs.legendLayer.___layerContainer___.childNodes.length).toBe(0);
             });
 
             it('dragging', () => {
-                let value = '';
+                let dom = realRender(Schedule, {});
+                let drapLayer = dom.refs.container.childNodes[0].childNodes[3];
+                expect(drapLayer.className).toBe('drag-layer');
+                TestUtils.Simulate.mouseDown(drapLayer, {clientX: 40, clientY: 40});
+                let evt = document.createEvent('MouseEvents');
+                evt.initEvent('mousemove', true, false, undefined, undefined, undefined, undefined, 50, 100);
+                window.dispatchEvent(evt);
+                evt.initEvent('mouseup', true, false, undefined, undefined, undefined, undefined, 50, 100);
+                window.dispatchEvent(evt);
+            });
+
+            it('disabled', () => {
+                let dom = realRender(Schedule, {disabled: true});
+                dom.onOptMouseMove({clientX: 10, clientY: 10});
+                expect(dom.refs.legendLayer.___layerContainer___.childNodes.length).toBe(0);
+                dom.onOptMouseMove({clientX: 50, clientY: 10});
+                expect(dom.refs.legendLayer.___layerContainer___.childNodes.length).toBe(0);
+                dom.onOptMouseMove({clientX: 50, clientY: 80});
+                expect(dom.refs.legendLayer.___layerContainer___.childNodes.length).toBe(0);
+                dom.onOptMouseOut();
+                expect(dom.refs.legendLayer.___layerContainer___.childNodes.length).toBe(0);
+                let drapLayer = dom.refs.container.childNodes[0].childNodes[3];
+                TestUtils.Simulate.mouseDown(drapLayer, {clientX: 40, clientY: 40});
+                let evt = document.createEvent('MouseEvents');
+                evt.initEvent('mousemove', true, false, undefined, undefined, undefined, undefined, 50, 100);
+                window.dispatchEvent(evt);
+                evt.initEvent('mouseup', true, false, undefined, undefined, undefined, undefined, 50, 100);
+                window.dispatchEvent(evt);
+                dom.onDrop(1, 1, {});
+            });
+
+            it('selector', () => {
                 let dom = realRender(Schedule, {
-                    onChange(e) {
-                        value = e.target.value;
-                    }
+                    value: '',
+                    flags: {enableRowSelector: true, enableColumnSelector: true}
                 });
-                expect(dom.refs.container.childNodes[0].childNodes[3].className).toBe('drag-layer');
-                //TestUtils.Simulate.click
+                expect(dom.refs.container.childNodes.length).toBe(5);
+                expect(dom.refs.container.childNodes[0].childNodes.length).toBe(24);
+                TestUtils.Simulate.change(dom.refs.container.childNodes[0].childNodes[0].childNodes[0], {target: {
+                    checked: true
+                }});
+                TestUtils.Simulate.change(dom.refs.container.childNodes[2].childNodes[1].childNodes[0], {target: {
+                    checked: true
+                }});
             });
 
         });
