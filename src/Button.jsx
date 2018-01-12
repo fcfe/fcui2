ac221bd01f943e92b5bb27874eb4730860eaa170
lihaitao03 <lihaitao03@baidu.com>
2018-01-12 12:40:36 +0800

    remove email

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,6 @@
 /**
  * 按钮
  * @author Brian Li
- * @email lihaitao03@baidu.com
  * @version 2.5.0.0
  */
 import React, {Component} from 'react';
--- src/core/util.js
+++ src/core/util.js
@@ -1,7 +1,6 @@
 /**
  * 工具集
  * @author Brian Li
- * @email lihaitao03@baidu.com
  * @version 2.5.0.0
  */
 
